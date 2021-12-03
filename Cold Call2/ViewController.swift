469e62bca8b9371b9b408ebd609a7215e551b0ea
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-03 16:35:55 +0300

    Initial Commit

---
+++ Cold Call2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cold Call2
+//
+//  Created by Atheer Alahmari on 28/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
